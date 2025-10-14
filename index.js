ddf449f3b53a906ddf187de8b9cd418cb4aa72b9
Hammad Ahmed <hammadahmed682003@gmail.com>
2025-10-14 19:51:57 +0100

    Adjusted the full stop for for welcome heading on homepage.

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ http.createServer(function(req, res) {
      // if the user hasn't navigated to the about page - landing page
     }else {
         res.writeHead(200, { "Content-Type" : "text/html"});
-        res.end(`<h1>Welcome to the Node.js Web App</h1>. 
+        res.end(`<h1>Welcome to the Node.js Web App.</h1>
             <p>For About Me Section, click here: <a href="http://localhost:8000/about">About Me</a></p>
         `);
     }
